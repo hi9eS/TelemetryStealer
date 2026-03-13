2c5b6223a11f21409b7e66bb18b493505f9ba95e
hi9eS <neslonpauslon744@gmail.com>
2026-03-13 21:51:08 +0500

    Update Form1.Designer.vb

--- TelemetryStealer/Form1.Designer.vb
+++ TelemetryStealer/Form1.Designer.vb
@@ -1,14 +1,13 @@
 Imports System
 Imports System.Windows.Forms
-Imports System.Drawing
 
 <Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
 Partial Class Form1
+    Inherits Form
+
     Private components As System.ComponentModel.IContainer
-    Friend WithEvents Timer1 As Timer
     Friend WithEvents TabControl1 As TabControl
     Friend WithEvents TabPage1 As TabPage
-    Friend WithEvents TabPage2 As TabPage
     Friend WithEvents PanelStage As Panel
     Friend WithEvents LabelStage1 As Label
     Friend WithEvents LabelStage2 As Label
@@ -22,59 +21,33 @@ Partial Class Form1
     Friend WithEvents PitchLabel As Label
     Friend WithEvents YawLabel As Label
     Friend WithEvents RollLabel As Label
+    Friend WithEvents TabPage2 As TabPage
+    Friend WithEvents Timer1 As Timer
 
     Private Sub InitializeComponent()
         Me.components = New System.ComponentModel.Container()
-        Me.TabControl1 = New TabControl()
-        Me.TabPage1 = New TabPage()
-        Me.TabPage2 = New TabPage()
-        Me.PanelStage = New Panel()
-        Me.LabelStage1 = New Label()
-        Me.LabelStage2 = New Label()
-        Me.LabelStage3 = New Label()
-        Me.TimerPanel = New Panel()
-        Me.LabelTimer = New Label()
-        Me.PropPanel = New Panel()
-        Me.LabelProp = New Label()
-        Me.SpeedLabel = New Label()
-        Me.AltLabel = New Label()
-        Me.PitchLabel = New Label()
-        Me.YawLabel = New Label()
-        Me.RollLabel = New Label()
-        Me.Timer1 = New Timer(Me.components)
+        Me.TabControl1 = New System.Windows.Forms.TabControl()
+        Me.TabPage1 = New System.Windows.Forms.TabPage()
+        Me.PanelStage = New System.Windows.Forms.Panel()
+        Me.LabelStage1 = New System.Windows.Forms.Label()
+        Me.LabelStage2 = New System.Windows.Forms.Label()
+        Me.LabelStage3 = New System.Windows.Forms.Label()
+        Me.TimerPanel = New System.Windows.Forms.Panel()
+        Me.LabelTimer = New System.Windows.Forms.Label()
+        Me.PropPanel = New System.Windows.Forms.Panel()
+        Me.LabelProp = New System.Windows.Forms.Label()
+        Me.SpeedLabel = New System.Windows.Forms.Label()
+        Me.AltLabel = New System.Windows.Forms.Label()
+        Me.PitchLabel = New System.Windows.Forms.Label()
+        Me.YawLabel = New System.Windows.Forms.Label()
+        Me.RollLabel = New System.Windows.Forms.Label()
+        Me.TabPage2 = New System.Windows.Forms.TabPage()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
 
-        ' Настройка формы
-        Me.SuspendLayout()
-        Me.ClientSize = New Size(800, 600)
-        Me.Text = "Telemetry Stealer"
+        ' Настройка элементов (пример)
+        Me.Timer1.Interval = 1000
 
-        ' TabControl
-        Me.TabControl1.Dock = DockStyle.Fill
-        Me.TabControl1.Controls.Add(Me.TabPage1)
-        Me.TabControl1.Controls.Add(Me.TabPage2)
+        ' Добавление контролов и прочее здесь, если нужно
 
-        ' TabPage1
-        Me.TabPage1.Controls.AddRange({PanelStage, TimerPanel, PropPanel})
-        Me.TabPage2.Text = "Tab 2"
-
-        ' PanelStage
-        Me.PanelStage.Controls.AddRange({LabelStage1, LabelStage2, LabelStage3})
-
-        ' TimerPanel
-        Me.TimerPanel.Controls.Add(LabelTimer)
-
-        ' PropPanel
-        Me.PropPanel.Controls.AddRange({LabelProp, SpeedLabel, AltLabel, PitchLabel, YawLabel, RollLabel})
-
-        ' Labels default
-        Dim labels() As Label = {LabelStage1, LabelStage2, LabelStage3, LabelTimer, LabelProp, SpeedLabel, AltLabel, PitchLabel, YawLabel, RollLabel}
-        For Each lbl In labels
-            lbl.ForeColor = Color.Cyan
-            lbl.Font = New Font("Segoe UI Semibold", 14, FontStyle.Bold)
-        Next
-
-        ' Добавляем TabControl на форму
-        Me.Controls.Add(TabControl1)
-        Me.ResumeLayout(False)
     End Sub
 End Class
