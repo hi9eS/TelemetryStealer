dd7e910cb934ec66355985df6e189f085e58e539
hi9eS <neslonpauslon744@gmail.com>
2026-03-13 21:50:53 +0500

    Update Form1.vb

--- TelemetryStealer/Form1.vb
+++ TelemetryStealer/Form1.vb
@@ -1,34 +1,27 @@
 Imports System
 Imports System.Windows.Forms
-Imports System.Drawing
 
-<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
 Partial Class Form1
     Inherits Form
 
-    Public Sub New()
-        InitializeComponent()
-    End Sub
+    ' Таймер
+    Private WithEvents Timer1 As Timer
 
-    ' Событие загрузки формы
-    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
-        Timer1.Interval = 1000
+    ' События формы
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles Me.Load
         Timer1.Start()
     End Sub
 
-    ' Событие закрытия формы
-    Private Sub Form1_FormClosing(sender As Object, e As FormClosingEventArgs) Handles MyBase.FormClosing
+    Private Sub Form1_FormClosing(sender As Object, e As FormClosingEventArgs) Handles Me.FormClosing
         Timer1.Stop()
     End Sub
 
-    ' Событие тика таймера
     Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
-        LabelTimer.Text = DateTime.Now.ToString("HH:mm:ss")
+        ' Логика таймера
     End Sub
 
-    ' Обработка нажатий клавиш
     Protected Overrides Sub OnKeyDown(e As KeyEventArgs)
         MyBase.OnKeyDown(e)
-        ' Добавьте обработку клавиш здесь
+        ' Логика клавиш
     End Sub
 End Class
