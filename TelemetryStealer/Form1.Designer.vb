5d4165b4d450b4cf002f4706fd3fbeeb7c293a59
hi9eS <neslonpauslon744@gmail.com>
2026-03-13 21:15:14 +0500

    Update Form1.Designer.vb

--- TelemetryStealer/Form1.Designer.vb
+++ TelemetryStealer/Form1.Designer.vb
@@ -1,326 +1,128 @@
-﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
-Partial Class Form1
-    Inherits System.Windows.Forms.Form
+Private Sub InitializeComponent()
+    Me.components = New System.ComponentModel.Container()
+    Me.TabControl1 = New System.Windows.Forms.TabControl()
+    Me.TabPage1 = New System.Windows.Forms.TabPage()
+    Me.PanelStage = New System.Windows.Forms.Panel()
+    Me.LabelStage1 = New System.Windows.Forms.Label()
+    Me.LabelStage2 = New System.Windows.Forms.Label()
+    Me.LabelStage3 = New System.Windows.Forms.Label()
+    Me.TimerPanel = New System.Windows.Forms.Panel()
+    Me.LabelTimer = New System.Windows.Forms.Label()
+    Me.PropPanel = New System.Windows.Forms.Panel()
+    Me.LabelProp = New System.Windows.Forms.Label()
+    Me.SpeedLabel = New System.Windows.Forms.Label()
+    Me.AltLabel = New System.Windows.Forms.Label()
+    Me.PitchLabel = New System.Windows.Forms.Label()
+    Me.YawLabel = New System.Windows.Forms.Label()
+    Me.RollLabel = New System.Windows.Forms.Label()
+    Me.TabPage2 = New System.Windows.Forms.TabPage()
+    Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
 
-    'Form overrides dispose to clean up the component list.
-    <System.Diagnostics.DebuggerNonUserCode()>
-    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
-        Try
-            If disposing AndAlso components IsNot Nothing Then
-                components.Dispose()
-            End If
-        Finally
-            MyBase.Dispose(disposing)
-        End Try
-    End Sub
+    ' TabControl
+    Me.TabControl1.Alignment = TabAlignment.Left
+    Me.TabControl1.Controls.Add(Me.TabPage1)
+    Me.TabControl1.Controls.Add(Me.TabPage2)
+    Me.TabControl1.Location = New Point(0, 0)
+    Me.TabControl1.Size = New Size(800, 480)
+    Me.TabControl1.Multiline = True
 
-    'Required by the Windows Form Designer
-    Private components As System.ComponentModel.IContainer
+    ' TabPage1 - Launch HUD
+    Me.TabPage1.BackColor = Color.FromArgb(25, 25, 25) ' прозрачный тёмный
+    Me.TabPage1.Controls.Add(Me.PanelStage)
+    Me.TabPage1.Controls.Add(Me.TimerPanel)
+    Me.TabPage1.Controls.Add(Me.PropPanel)
+    Me.TabPage1.Controls.Add(Me.SpeedLabel)
+    Me.TabPage1.Controls.Add(Me.AltLabel)
+    Me.TabPage1.Controls.Add(Me.PitchLabel)
+    Me.TabPage1.Controls.Add(Me.YawLabel)
+    Me.TabPage1.Controls.Add(Me.RollLabel)
 
-    'NOTE: The following procedure is required by the Windows Form Designer
-    'It can be modified using the Windows Form Designer.  
-    'Do not modify it using the code editor.
-    <System.Diagnostics.DebuggerStepThrough()>
-    Private Sub InitializeComponent()
-        Me.components = New System.ComponentModel.Container()
-        Me.TabControl1 = New System.Windows.Forms.TabControl()
-        Me.TabPage1 = New System.Windows.Forms.TabPage()
-        Me.Panel1 = New System.Windows.Forms.Panel()
-        Me.PiLa = New System.Windows.Forms.Label()
-        Me.Label6 = New System.Windows.Forms.Label()
-        Me.MaLa = New System.Windows.Forms.Label()
-        Me.Label4 = New System.Windows.Forms.Label()
-        Me.FuLa = New System.Windows.Forms.Label()
-        Me.Label333 = New System.Windows.Forms.Label()
-        Me.PeLa = New System.Windows.Forms.Label()
-        Me.ApLa = New System.Windows.Forms.Label()
-        Me.Label2 = New System.Windows.Forms.Label()
-        Me.VelLa = New System.Windows.Forms.Label()
-        Me.Label3 = New System.Windows.Forms.Label()
-        Me.AltLa = New System.Windows.Forms.Label()
-        Me.Label1 = New System.Windows.Forms.Label()
-        Me.TabPage2 = New System.Windows.Forms.TabPage()
-        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
-        Me.Timer2 = New System.Windows.Forms.Timer(Me.components)
-        Me.Label5 = New System.Windows.Forms.Label()
-        Me.Label7 = New System.Windows.Forms.Label()
-        Me.YaLa = New System.Windows.Forms.Label()
-        Me.RollLab = New System.Windows.Forms.Label()
-        Me.TabControl1.SuspendLayout()
-        Me.TabPage1.SuspendLayout()
-        Me.SuspendLayout()
-        '
-        'TabControl1
-        '
-        Me.TabControl1.Alignment = System.Windows.Forms.TabAlignment.Left
-        Me.TabControl1.Controls.Add(Me.TabPage1)
-        Me.TabControl1.Controls.Add(Me.TabPage2)
-        Me.TabControl1.Location = New System.Drawing.Point(1, 12)
-        Me.TabControl1.Multiline = True
-        Me.TabControl1.Name = "TabControl1"
-        Me.TabControl1.SelectedIndex = 0
-        Me.TabControl1.Size = New System.Drawing.Size(635, 372)
-        Me.TabControl1.TabIndex = 0
-        '
-        'TabPage1
-        '
-        Me.TabPage1.Controls.Add(Me.RollLab)
-        Me.TabPage1.Controls.Add(Me.YaLa)
-        Me.TabPage1.Controls.Add(Me.Label7)
-        Me.TabPage1.Controls.Add(Me.Label5)
-        Me.TabPage1.Controls.Add(Me.Panel1)
-        Me.TabPage1.Controls.Add(Me.PiLa)
-        Me.TabPage1.Controls.Add(Me.Label6)
-        Me.TabPage1.Controls.Add(Me.MaLa)
-        Me.TabPage1.Controls.Add(Me.Label4)
-        Me.TabPage1.Controls.Add(Me.FuLa)
-        Me.TabPage1.Controls.Add(Me.Label333)
-        Me.TabPage1.Controls.Add(Me.PeLa)
-        Me.TabPage1.Controls.Add(Me.ApLa)
-        Me.TabPage1.Controls.Add(Me.Label2)
-        Me.TabPage1.Controls.Add(Me.VelLa)
-        Me.TabPage1.Controls.Add(Me.Label3)
-        Me.TabPage1.Controls.Add(Me.AltLa)
-        Me.TabPage1.Controls.Add(Me.Label1)
-        Me.TabPage1.Location = New System.Drawing.Point(25, 4)
-        Me.TabPage1.Name = "TabPage1"
-        Me.TabPage1.Padding = New System.Windows.Forms.Padding(3)
-        Me.TabPage1.Size = New System.Drawing.Size(606, 364)
-        Me.TabPage1.TabIndex = 0
-        Me.TabPage1.Text = "Launch"
-        Me.TabPage1.UseVisualStyleBackColor = True
-        '
-        'Panel1
-        '
-        Me.Panel1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center
-        Me.Panel1.Location = New System.Drawing.Point(207, 76)
-        Me.Panel1.Name = "Panel1"
-        Me.Panel1.Size = New System.Drawing.Size(200, 200)
-        Me.Panel1.TabIndex = 13
-        '
-        'PiLa
-        '
-        Me.PiLa.Font = New System.Drawing.Font("Segoe UI", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.PiLa.Location = New System.Drawing.Point(222, 336)
-        Me.PiLa.Name = "PiLa"
-        Me.PiLa.Size = New System.Drawing.Size(175, 25)
-        Me.PiLa.TabIndex = 12
-        Me.PiLa.Text = "15º"
-        Me.PiLa.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
-        '
-        'Label6
-        '
-        Me.Label6.AutoSize = True
-        Me.Label6.Font = New System.Drawing.Font("Segoe UI", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Label6.Location = New System.Drawing.Point(277, 311)
-        Me.Label6.Name = "Label6"
-        Me.Label6.Size = New System.Drawing.Size(56, 25)
-        Me.Label6.TabIndex = 11
-        Me.Label6.Text = "Pitch"
-        '
-        'MaLa
-        '
-        Me.MaLa.Font = New System.Drawing.Font("Segoe UI", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.MaLa.Location = New System.Drawing.Point(3, 283)
-        Me.MaLa.Name = "MaLa"
-        Me.MaLa.Size = New System.Drawing.Size(134, 25)
-        Me.MaLa.TabIndex = 10
-        Me.MaLa.Text = "0g"
-        Me.MaLa.TextAlign = System.Drawing.ContentAlignment.MiddleLeft
-        '
-        'Label4
-        '
-        Me.Label4.AutoSize = True
-        Me.Label4.Font = New System.Drawing.Font("Segoe UI", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Label4.Location = New System.Drawing.Point(2, 258)
-        Me.Label4.Name = "Label4"
-        Me.Label4.Size = New System.Drawing.Size(104, 25)
-        Me.Label4.TabIndex = 9
-        Me.Label4.Text = "Total Mass"
-        '
-        'FuLa
-        '
-        Me.FuLa.Font = New System.Drawing.Font("Segoe UI", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.FuLa.Location = New System.Drawing.Point(513, 277)
-        Me.FuLa.Name = "FuLa"
-        Me.FuLa.Size = New System.Drawing.Size(90, 25)
-        Me.FuLa.TabIndex = 8
-        Me.FuLa.Text = "0.00%"
-        Me.FuLa.TextAlign = System.Drawing.ContentAlignment.MiddleRight
-        '
-        'Label333
-        '
-        Me.Label333.AutoSize = True
-        Me.Label333.Font = New System.Drawing.Font("Segoe UI", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Label333.Location = New System.Drawing.Point(554, 252)
-        Me.Label333.Name = "Label333"
-        Me.Label333.Size = New System.Drawing.Size(49, 25)
-        Me.Label333.TabIndex = 7
-        Me.Label333.Text = "Fuel"
-        '
-        'PeLa
-        '
-        Me.PeLa.AutoSize = True
-        Me.PeLa.Font = New System.Drawing.Font("Segoe UI", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.PeLa.Location = New System.Drawing.Point(3, 53)
-        Me.PeLa.Name = "PeLa"
-        Me.PeLa.Size = New System.Drawing.Size(38, 25)
-        Me.PeLa.TabIndex = 6
-        Me.PeLa.Text = "0m"
-        Me.PeLa.TextAlign = System.Drawing.ContentAlignment.MiddleRight
-        '
-        'ApLa
-        '
-        Me.ApLa.AutoSize = True
-        Me.ApLa.Font = New System.Drawing.Font("Segoe UI", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.ApLa.Location = New System.Drawing.Point(3, 28)
-        Me.ApLa.Name = "ApLa"
-        Me.ApLa.Size = New System.Drawing.Size(74, 25)
-        Me.ApLa.TabIndex = 5
-        Me.ApLa.Text = "0m - 0s"
-        Me.ApLa.TextAlign = System.Drawing.ContentAlignment.MiddleRight
-        '
-        'Label2
-        '
-        Me.Label2.AutoSize = True
-        Me.Label2.Font = New System.Drawing.Font("Segoe UI", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Label2.Location = New System.Drawing.Point(3, 3)
-        Me.Label2.Name = "Label2"
-        Me.Label2.Size = New System.Drawing.Size(66, 25)
-        Me.Label2.TabIndex = 4
-        Me.Label2.Text = "Ap/Pe"
-        '
-        'VelLa
-        '
-        Me.VelLa.Font = New System.Drawing.Font("Segoe UI", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.VelLa.Location = New System.Drawing.Point(454, 336)
-        Me.VelLa.Name = "VelLa"
-        Me.VelLa.Size = New System.Drawing.Size(152, 25)
-        Me.VelLa.TabIndex = 3
-        Me.VelLa.Text = "0m/s"
-        Me.VelLa.TextAlign = System.Drawing.ContentAlignment.MiddleRight
-        '
-        'Label3
-        '
-        Me.Label3.AutoSize = True
-        Me.Label3.Font = New System.Drawing.Font("Segoe UI", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Label3.Location = New System.Drawing.Point(524, 311)
-        Me.Label3.Name = "Label3"
-        Me.Label3.Size = New System.Drawing.Size(82, 25)
-        Me.Label3.TabIndex = 2
-        Me.Label3.Text = "Velocity"
-        '
-        'AltLa
-        '
-        Me.AltLa.AutoSize = True
-        Me.AltLa.Font = New System.Drawing.Font("Segoe UI", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.AltLa.Location = New System.Drawing.Point(3, 336)
-        Me.AltLa.Name = "AltLa"
-        Me.AltLa.Size = New System.Drawing.Size(38, 25)
-        Me.AltLa.TabIndex = 1
-        Me.AltLa.Text = "0m"
-        Me.AltLa.TextAlign = System.Drawing.ContentAlignment.MiddleRight
-        '
-        'Label1
-        '
-        Me.Label1.AutoSize = True
-        Me.Label1.Font = New System.Drawing.Font("Segoe UI", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Label1.Location = New System.Drawing.Point(3, 311)
-        Me.Label1.Name = "Label1"
-        Me.Label1.Size = New System.Drawing.Size(83, 25)
-        Me.Label1.TabIndex = 0
-        Me.Label1.Text = "Altitude"
-        '
-        'TabPage2
-        '
-        Me.TabPage2.Location = New System.Drawing.Point(25, 4)
-        Me.TabPage2.Name = "TabPage2"
-        Me.TabPage2.Padding = New System.Windows.Forms.Padding(3)
-        Me.TabPage2.Size = New System.Drawing.Size(606, 364)
-        Me.TabPage2.TabIndex = 1
-        Me.TabPage2.Text = "Landing"
-        Me.TabPage2.UseVisualStyleBackColor = True
-        '
-        'Timer1
-        '
-        Me.Timer1.Enabled = True
-        '
-        'Label5
-        '
-        Me.Label5.AutoSize = True
-        Me.Label5.Font = New System.Drawing.Font("Segoe UI", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Label5.Location = New System.Drawing.Point(554, 3)
-        Me.Label5.Name = "Label5"
-        Me.Label5.Size = New System.Drawing.Size(46, 25)
-        Me.Label5.TabIndex = 14
-        Me.Label5.Text = "Roll"
-        '
-        'Label7
-        '
-        Me.Label7.AutoSize = True
-        Me.Label7.Font = New System.Drawing.Font("Segoe UI", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Label7.Location = New System.Drawing.Point(551, 142)
-        Me.Label7.Name = "Label7"
-        Me.Label7.Size = New System.Drawing.Size(47, 25)
-        Me.Label7.TabIndex = 15
-        Me.Label7.Text = "Yaw"
-        '
-        'YaLa
-        '
-        Me.YaLa.Font = New System.Drawing.Font("Segoe UI", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.YaLa.Location = New System.Drawing.Point(510, 167)
-        Me.YaLa.Name = "YaLa"
-        Me.YaLa.Size = New System.Drawing.Size(90, 25)
-        Me.YaLa.TabIndex = 16
-        Me.YaLa.Text = "0º"
-        Me.YaLa.TextAlign = System.Drawing.ContentAlignment.MiddleRight
-        '
-        'RollLab
-        '
-        Me.RollLab.Font = New System.Drawing.Font("Segoe UI", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.RollLab.Location = New System.Drawing.Point(510, 28)
-        Me.RollLab.Name = "RollLab"
-        Me.RollLab.Size = New System.Drawing.Size(90, 25)
-        Me.RollLab.TabIndex = 17
-        Me.RollLab.Text = "0º"
-        Me.RollLab.TextAlign = System.Drawing.ContentAlignment.MiddleRight
-        '
-        'Form1
-        '
-        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
-        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(648, 396)
-        Me.Controls.Add(Me.TabControl1)
-        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
-        Me.Name = "Form1"
-        Me.Text = "Telemetry Getter"
-        Me.TabControl1.ResumeLayout(False)
-        Me.TabPage1.ResumeLayout(False)
-        Me.TabPage1.PerformLayout()
-        Me.ResumeLayout(False)
+    ' PanelStage - этапы
+    Me.PanelStage.BackColor = Color.FromArgb(100, 0, 45, 105)
+    Me.PanelStage.Location = New Point(150, 10)
+    Me.PanelStage.Size = New Size(500, 40)
+    Me.PanelStage.BorderStyle = BorderStyle.FixedSingle
+    Me.PanelStage.Controls.Add(Me.LabelStage1)
+    Me.PanelStage.Controls.Add(Me.LabelStage2)
+    Me.PanelStage.Controls.Add(Me.LabelStage3)
 
-    End Sub
-    Friend WithEvents TabControl1 As TabControl
-    Friend WithEvents TabPage1 As TabPage
-    Friend WithEvents TabPage2 As TabPage
-    Friend WithEvents Timer1 As Timer
-    Friend WithEvents AltLa As Label
-    Friend WithEvents Label1 As Label
-    Friend WithEvents VelLa As Label
-    Friend WithEvents Label3 As Label
-    Friend WithEvents PeLa As Label
-    Friend WithEvents ApLa As Label
-    Friend WithEvents Label2 As Label
-    Friend WithEvents PiLa As Label
-    Friend WithEvents Label6 As Label
-    Friend WithEvents MaLa As Label
-    Friend WithEvents Label4 As Label
-    Friend WithEvents FuLa As Label
-    Friend WithEvents Label333 As Label
-    Friend WithEvents Panel1 As Panel
-    Friend WithEvents Timer2 As Timer
-    Friend WithEvents RollLab As Label
-    Friend WithEvents YaLa As Label
-    Friend WithEvents Label7 As Label
-    Friend WithEvents Label5 As Label
-End Class
+    ' Stage Labels
+    Dim stages = {Me.LabelStage1, Me.LabelStage2, Me.LabelStage3}
+    Dim names = {"STARTUP", "LIFTOFF", "MAX Q"}
+    For i As Integer = 0 To 2
+        stages(i).Text = names(i)
+        stages(i).Font = New Font("Segoe UI Semibold", 12, FontStyle.Bold)
+        stages(i).ForeColor = Color.Cyan
+        stages(i).Location = New Point(5 + i * 160, 5)
+        stages(i).Size = New Size(150, 30)
+        stages(i).TextAlign = ContentAlignment.MiddleCenter
+        stages(i).BackColor = Color.FromArgb(80, 0, 70, 120)
+    Next
+
+    ' TimerPanel
+    Me.TimerPanel.BackColor = Color.FromArgb(120, 0, 45, 105)
+    Me.TimerPanel.Location = New Point(250, 70)
+    Me.TimerPanel.Size = New Size(300, 60)
+    Me.TimerPanel.BorderStyle = BorderStyle.FixedSingle
+    Me.TimerPanel.Controls.Add(Me.LabelTimer)
+
+    Me.LabelTimer.Font = New Font("Segoe UI Semibold", 26, FontStyle.Bold)
+    Me.LabelTimer.ForeColor = Color.Cyan
+    Me.LabelTimer.Location = New Point(0, 0)
+    Me.LabelTimer.Size = New Size(300, 60)
+    Me.LabelTimer.Text = "T- 00:00:30"
+    Me.LabelTimer.TextAlign = ContentAlignment.MiddleCenter
+
+    ' PropPanel - топливо
+    Me.PropPanel.BackColor = Color.FromArgb(120, 0, 45, 105)
+    Me.PropPanel.Location = New Point(600, 400)
+    Me.PropPanel.Size = New Size(180, 50)
+    Me.PropPanel.BorderStyle = BorderStyle.FixedSingle
+    Me.PropPanel.Controls.Add(Me.LabelProp)
+
+    Me.LabelProp.Font = New Font("Segoe UI Semibold", 14, FontStyle.Bold)
+    Me.LabelProp.ForeColor = Color.Cyan
+    Me.LabelProp.Location = New Point(10, 10)
+    Me.LabelProp.Size = New Size(160, 30)
+    Me.LabelProp.Text = "PROPELLANT 3/3"
+    Me.LabelProp.TextAlign = ContentAlignment.MiddleCenter
+
+    ' Speed, Alt, Pitch, Yaw, Roll Labels
+    Me.SpeedLabel.Font = New Font("Segoe UI Semibold", 14, FontStyle.Bold)
+    Me.SpeedLabel.ForeColor = Color.Cyan
+    Me.SpeedLabel.Location = New Point(650, 50)
+    Me.SpeedLabel.Size = New Size(120, 30)
+    Me.SpeedLabel.Text = "0 m/s"
+
+    Me.AltLabel.Font = New Font("Segoe UI Semibold", 14, FontStyle.Bold)
+    Me.AltLabel.ForeColor = Color.Cyan
+    Me.AltLabel.Location = New Point(650, 90)
+    Me.AltLabel.Size = New Size(120, 30)
+    Me.AltLabel.Text = "0 m"
+
+    Me.PitchLabel.Font = New Font("Segoe UI Semibold", 14, FontStyle.Bold)
+    Me.PitchLabel.ForeColor = Color.Cyan
+    Me.PitchLabel.Location = New Point(50, 400)
+    Me.PitchLabel.Size = New Size(120, 30)
+    Me.PitchLabel.Text = "Pitch: 0º"
+
+    Me.YawLabel.Font = New Font("Segoe UI Semibold", 14, FontStyle.Bold)
+    Me.YawLabel.ForeColor = Color.Cyan
+    Me.YawLabel.Location = New Point(50, 440)
+    Me.YawLabel.Size = New Size(120, 30)
+    Me.YawLabel.Text = "Yaw: 0º"
+
+    Me.RollLabel.Font = New Font("Segoe UI Semibold", 14, FontStyle.Bold)
+    Me.RollLabel.ForeColor = Color.Cyan
+    Me.RollLabel.Location = New Point(50, 480)
+    Me.RollLabel.Size = New Size(120, 30)
+    Me.RollLabel.Text = "Roll: 0º"
+
+    ' Form1
+    Me.ClientSize = New Size(800, 480)
+    Me.Controls.Add(Me.TabControl1)
+    Me.FormBorderStyle = FormBorderStyle.None
+    Me.BackColor = Color.Black
+    Me.Name = "Form1"
+    Me.Text = "Telemetry HUD"
+End Sub
